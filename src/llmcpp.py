281053451f26f58340a8954b776be9ec58efb70d
thiagorr162 <thiagorr162@gmail.com>
2024-10-05 00:10:14 -0300

    Add code to run llamacpp

---
+++ src/llmcpp.py
@@ -0,0 +1,63 @@
+import multiprocessing
+import re
+
+from langchain_community.chat_models import ChatLlamaCpp
+from langchain_core.prompts import ChatPromptTemplate
+
+
+def split_document(document):
+    return re.split(r"\n\s*\n", document)
+
+
+file_path = "data/patente.txt"
+
+with open(file_path, "r") as file:
+    data = file.read()
+
+chunks = split_document(data)
+chunk = chunks[17]
+
+local_model = "models/Hermes-2-Pro-Llama-3-8B-Q4_K_M.gguf"
+
+
+llm = ChatLlamaCpp(
+    temperature=0.5,
+    model_path=local_model,
+    n_ctx=2040,
+    # n_gpu_layers=8,
+    # n_batch=10,  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
+    # max_tokens=1024,
+    n_threads=multiprocessing.cpu_count() - 2,
+    # repeat_penalty=1.5,
+    # top_p=0.5,
+    verbose=True,
+)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Given the document related to glasses,"
+            "your task is to analyze the document and extract the chemical composition of all glasses"
+            "described in the document, detailing the percentage of each chemical element, and "
+            "listing the key properties associated with each glass."
+            "If there is no relevant information, return 'there is no information'."
+            "Here are the contents of the document:\n\n----\n\n{document}.",
+        ),
+        ("human", "{input}"),
+    ]
+)
+
+chain = prompt | llm
+
+ai_msg = chain.invoke(
+    {
+        "document": chunk,  # Passa o pedaço do documento
+        "input": "Respond to me in JSON format. List the chemical compositions of the glass mentioned in the document"
+        "along with the percentage of each element, and the key properties associated with each glass type. ",
+    }
+)
+print(ai_msg.content)
+
+breakpoint()
