7a1e203ffa588737ba41de72fb9a456e2a5a7ac7
thiagorr162 <thiagorr162@gmail.com>
2024-10-06 02:06:44 -0300

    Add script to download claims from patents

--- src/scraping/get_patents.py
+++ src/scraping/get_patents.py
@@ -1,5 +1,55 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
+from pathlib import Path
+
+def extract_patent_id_from_url(url):
+    # Extrai o ID da patente da URL (última parte após a "/")
+    return url.split("/")[-1].split(".")[0]
+
+
+def extract_claims_from_html(url):
+    try:
+        # Acessa a página da patente
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # Faz o parsing do conteúdo HTML da página
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Encontra o elemento <div> que contém "Claims:"
+        claims_div = soup.find('div', class_='disp_elm_title', string='Claims:')
+
+        if claims_div:
+            # Acha o próximo conteúdo após o div "Claims:"
+            claims_section = claims_div.find_next_sibling()
+            claims_text = claims_section.get_text(strip=True) if claims_section else "Seção de Claims não encontrada."
+
+            # Estrutura do JSON a ser salvo
+            patent_data = {
+                "url": url,
+                "claims": claims_text
+            }
+
+            # Cria o diretório usando pathlib
+            output_dir = Path("data/patents/")
+            output_dir.mkdir(parents=True, exist_ok=True)
+
+            # Extrai o ID da patente da URL e salva o arquivo com esse nome
+            patent_id = extract_patent_id_from_url(url)
+            json_file_path = output_dir / f"{patent_id}.json"
+
+            # Salva os dados no formato JSON
+            with open(json_file_path, "w") as json_file:
+                json.dump(patent_data, json_file, indent=4)
+
+            print(f"Dados da patente extraídos com sucesso e salvos em '{json_file_path}'.")
+        else:
+            print("Elemento 'Claims:' não encontrado na página.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao acessar a página: {e}")
 
 
 def get_patent_links(search_url):
@@ -19,9 +69,19 @@ def get_patent_links(search_url):
 
     return links
 
-page=1
+
+page = 1
 search_url = f"https://www.freepatentsonline.com/result.html?p={page}&sort=relevance&srch=top&query_txt=glass+composition&patents_us=on"
 
 patent_links = get_patent_links(search_url)
 
-breakpoint()
+for pat in patent_links:
+    patent_id = extract_patent_id_from_url(pat)
+    json_file_path = Path(f"data/patents/{patent_id}.json")
+    
+    # Verifica se o arquivo já existe
+    if not json_file_path.exists():
+        extract_claims_from_html(pat)
+    else:
+        print(f"Arquivo {json_file_path} já existe. Pulando extração.")
+
