9ed82f7b6a586f84738d8ef721700842125eac7d
thiagorr162 <thiagorr162@gmail.com>
2024-10-06 02:32:16 -0300

    Adapt script to run patent claims

--- src/llm/run_cppllama.py
+++ src/llm/run_cppllama.py
@@ -1,28 +1,27 @@
+import json
 import multiprocessing
-import re
+from pathlib import Path
 
 from langchain_community.chat_models import ChatLlamaCpp
 from langchain_core.prompts import ChatPromptTemplate
 
-
-def split_document(document):
-    return re.split(r"\n\s*\n", document)
-
-
-file_path = "data/patente.txt"
 grammar_path = "grammars/glasses.gbnf"
-
-with open(file_path, "r") as file:
-    data = file.read()
 
 with open(grammar_path, "r") as file:
     grammar = file.read()
 
-chunks = split_document(data)
-chunk = chunks[17]
+json_directory = Path("data/patents")
+# Iterar por todos os arquivos JSON no diretório
+for json_file in json_directory.glob("*.json"):
+    with open(json_file, "r", encoding="utf-8") as f:
+        # Ler o conteúdo do arquivo JSON
+        data = json.load(f)
+        data = data["claims"]
+        # Adicionar o conteúdo à lista
+
+        break
 
 local_model = "models/Hermes-2-Pro-Llama-3-8B-Q8_0.gguf"
-
 
 llm = ChatLlamaCpp(
     temperature=0.8,
@@ -30,7 +29,7 @@ llm = ChatLlamaCpp(
     n_ctx=10000,
     # n_gpu_layers=8,
     # n_batch=10,  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
-    max_tokens=5000,
+    # max_tokens=10000,
     n_threads=multiprocessing.cpu_count() - 2,
     # repeat_penalty=1.5,
     # top_p=0.5,
@@ -38,15 +37,16 @@ llm = ChatLlamaCpp(
     grammar=grammar,
 )
 
+
 prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a helpful assistant. Given the document related to glasses, "
-            "your task is to analyze the document and extract the chemical composition of all glasses "
-            "described in the document, detailing the percentage of each chemical element and the refractive index "
-            "of each glass. If there is no relevant information, return 'there is no information'."
-            "Here are the contents of the document:\n\n----\n\n{document}.",
+            "You are a knowledgeable assistant. Your task is to analyze the provided document "
+            "related to glass compositions. Extract the chemical composition of each glass described, "
+            "including the percentage of each chemical element and any relevant properties, and organize "
+            "the information in a structured JSON format. "
+            "Below is the document content:\n\n----\n\n{document}.",
         ),
         (
             "human",
@@ -54,34 +54,19 @@ prompt = ChatPromptTemplate.from_messages(
         ),
     ]
 )
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant. Given the document related to glasses,"
-            "your task is to analyze the document and extract the chemical composition of all glasses"
-            "described in the document, detailing the percentage of each chemical element, and "
-            "its refractive index."
-            "Here are the contents of the document:\n\n----\n\n{document}.",
-        ),
-        (
-            "human",
-            "{input}",
-        ),
-    ]
-)
+
 
 chain = prompt | llm
 
 ai_msg = chain.invoke(
     {
-        "document": chunk,  # Passa o pedaço do documento
-        "input": "List the chemical compositions of the glass mentioned in the document"
-        "along with its refractive index. ",
+        "document": data,  # passa o pedaço do documento
+        "input": (
+            "List the chemical compositions of the glass mentioned in the document, "
+            "along with any relevant properties."
+        ),
     }
 )
-
-
 print(ai_msg.content)
 
 breakpoint()
