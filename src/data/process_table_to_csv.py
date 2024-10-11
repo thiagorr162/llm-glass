5f8bf0884feffed4eb3ec79b513969e0bfd60a61
thiagorr162 <thiagorr162@gmail.com>
2024-10-10 21:39:10 -0300

    Several changes
    - Save processed csv in an folder named after patent id
    - Remove lines with few commas

--- src/data/process_table_to_csv.py
+++ src/data/process_table_to_csv.py
@@ -16,9 +16,14 @@ def process_file(filepath):
     # Quebra o conteúdo em seções de tabelas
     tables = content.split("\n\n")  # Separação por linha em branco
 
-    # Variável para controlar o número da tabela
+    # Extrair o ID da patente da subpasta do arquivo
+    patent_id = filepath.parent.name
     table_number = filepath.stem.split("_")[1]
     table_count = 0
+
+    # Criar subpasta de saída com o ID da patente
+    patent_output_folder = output_folder / patent_id
+    patent_output_folder.mkdir(parents=True, exist_ok=True)
 
     # Processar cada tabela separadamente
     for table in tables:
@@ -26,14 +31,23 @@ def process_file(filepath):
 
         # Verifica se tem mais de 10 linhas (para garantir que seja uma tabela válida)
         if len(lines) > 10:
-            output_filepath = output_folder / f"table_{table_number}_{table_count}.csv"
+            # Conta a quantidade de vírgulas em cada linha
+            # comma_counts = [line.count(",") for line in lines]
 
-            # Escreve diretamente no arquivo CSV
-            with output_filepath.open("w") as csv_file:
-                for line in lines:
-                    csv_file.write(line + "\n")  # Escreve cada linha diretamente no arquivo CSV
+            # Identifica a linha que deve ser removida, se tiver menos vírgulas que as outras
+            # max_commas = max(comma_counts)
+            filtered_lines = [line for line in lines if line.count(",") > 1]
 
-            table_count += 1
+            # Verifica se ainda existem linhas válidas após a filtragem
+            if filtered_lines:
+                output_filepath = patent_output_folder / f"table_{table_number}_{table_count}.csv"
+
+                # Escreve diretamente no arquivo CSV
+                with output_filepath.open("w") as csv_file:
+                    for line in filtered_lines:
+                        csv_file.write(line + "\n")  # Escreve cada linha diretamente no arquivo CSV
+
+                table_count += 1
 
 
 # Processar todos os arquivos em subpastas da pasta de entrada
