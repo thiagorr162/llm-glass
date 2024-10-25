409146bb26c89a078b262e99de868ae491e59974
thiagorr162 <thiagorr162@gmail.com>
2024-10-25 00:11:18 -0300

    Add script to merge dataframes

---
+++ src/data/merge_tables.py
@@ -0,0 +1,22 @@
+import pathlib
+
+import pandas as pd
+
+input_path = pathlib.Path("data/patents")
+
+output_path = pathlib.Path("data/patents")
+
+properties_file = pathlib.Path("json/properties.json")
+
+
+all_dfs = []
+
+for table_file in input_path.rglob("*/processed/dataframe/*.csv"):
+    df = pd.read_csv(table_file)
+
+    all_dfs.append(df)
+
+
+merged_df = pd.concat(all_dfs, join="outer", ignore_index=True)
+
+merged_df.to_csv(output_path / "merged_df.csv")
