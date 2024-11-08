ccfcdf2e285327ef60abfbba82200d3d784f091f
tyba2 <thomazaguena@estudante.ufscar.br>
2024-11-07 21:45:55 -0300

    Update get_patent_info.py

--- src/scraping/get_patent_info.py
+++ src/scraping/get_patent_info.py
@@ -4,18 +4,17 @@ from pathlib import Path
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.service import Service
-
 
 parser = argparse.ArgumentParser(
     description="Busca de patentes no Google Patents com base em palavras-chave e número de páginas."
 )
 
-# Argumento para o número de páginas
 parser.add_argument(
     "--pages",
     "-p",
@@ -30,7 +29,6 @@ parser.add_argument(
     default=10,
     help="Seconds to wait to load patents.",
 )
-# Argumento para as palavras-chave
 parser.add_argument(
     "--keywords",
     "-k",
@@ -62,16 +60,17 @@ country = args.country
 all_urls = []
 
 geckodriver_path = args.selenium_path
-driver_service = webdriver.FirefoxService(executable_path=geckodriver_path)
-
 service = Service(executable_path=geckodriver_path)
-browser = webdriver.Firefox(service=service)
 
+firefox_binary_path = "C:/Program Files/Mozilla Firefox/firefox.exe"  
+options = Options()
+options.binary = firefox_binary_path
+
+browser = webdriver.Firefox(service=service, options=options)
 
 for page in range(0, pages + 1):
     print(f"Getting links for page: {page}")
 
-    # Construindo a URL de busca com múltiplos parâmetros de consulta
     search_url = (
         "https://patents.google.com/?q=" + "&q=".join([f"TI%3d({keyword})" for keyword in keywords]) + "&sort=new"
     )
@@ -84,13 +83,10 @@ for page in range(0, pages + 1):
 
     browser.get(search_url)
 
-    # Definir um tempo de espera até que os resultados estejam disponíveis
     wait = WebDriverWait(browser, args.wait)
 
-    # Localizar os elementos que contêm as patentes
     patent_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "search-result-item")))
 
-    # Capturar os URLs das patentes
     for patent in patent_elements:
         state_modifier = patent.find_element(By.CSS_SELECTOR, "state-modifier")
         data_result = state_modifier.get_attribute("data-result")
@@ -99,7 +95,6 @@ for page in range(0, pages + 1):
 
 print("All links done!\n")
 
-# Criar o diretório data/patents se ele não existir
 output_dir = Path("data/patents")
 output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -111,18 +106,14 @@ for url in all_urls:
 
     output_file = output_path / (file_name + ".json")
 
-    # Verificar se o arquivo já existe
     if output_file.exists():
         print(f"Arquivo {output_file} já existe. Pulando a criação.")
         continue
 
-    # Navegar para a URL completa da patente
     browser.get(url)
 
-    # Esperar até que a nova página de patente seja carregada
     wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
-    # Função auxiliar para verificar a existência de elementos
     def get_meta_content(selector):
         elements = browser.find_elements(By.CSS_SELECTOR, selector)
         if elements:
@@ -133,7 +124,6 @@ for url in all_urls:
                 return result
         return None
 
-    # Capturar os dados com base na presença dos elementos
     title = get_meta_content("meta[name='DC.title']")
     patent_type = get_meta_content("meta[name='DC.type']")
     description = get_meta_content("meta[name='DC.description']")
@@ -141,7 +131,6 @@ for url in all_urls:
     publication_number = get_meta_content("meta[name='citation_patent_publication_number']")
     pdf_url = get_meta_content("meta[name='citation_pdf_url']")
 
-    # Para inventores, retornar uma lista de nomes
     inventors = get_meta_content("meta[scheme='inventor']")
     assignee = get_meta_content("meta[scheme='assignee']")
     date = get_meta_content("meta[name='DC.date']")
@@ -160,7 +149,6 @@ for url in all_urls:
         print(f"No patent tables found on {url}.")
         html_tables = []
 
-    # Criar um dicionário com os dados
     patent_data = {
         "url": url,
         "title": title,
@@ -175,12 +163,7 @@ for url in all_urls:
         "html_tables": html_tables,
     }
 
-    # Gerar o nome do arquivo com base na URL (remover caracteres inválidos)
-
-    # Salvar os dados em um arquivo JSON
     with output_file.open(mode="w", encoding="utf-8") as f:
         json.dump(patent_data, f, ensure_ascii=False, indent=4)
 
-
-# Fechar o navegador ao final (se necessário)
 browser.quit()
